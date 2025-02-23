2296222b319d305bce1723c5407d39cdf873d83a
Alex Bieth <alex.bieth@gmail.com>
2025-02-23 17:48:38 +0100

    add top 10

--- main.py
+++ main.py
@@ -47,8 +47,8 @@ df = df.sort_values(by="Rank")
 st.markdown("### Top ATP Stats country and age")
 top_n = st.radio(
     "Select top N players",
-    options=[50, 100, 200, 500, 1000],
-    index=4,  # Default to top 1000
+    options=[10,50, 100, 200, 500, 1000],
+    index=2,  # Default to top 1000
     horizontal=True
 )
 
