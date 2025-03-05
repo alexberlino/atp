5fa7cca99177f1cc0b6f066b6e97b45155ea04d6
Alex Bieth <alex.bieth@gmail.com>
2025-03-05 19:06:39 +0100

    check

--- ranking.py
+++ ranking.py
@@ -1,5 +1,9 @@
 import shutil
 import undetected_chromedriver as uc
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -10,25 +14,21 @@ import time
 import re
 import pandas as pd
 import subprocess
-from datetime import datetime
-import undetected_chromedriver as uc
-
-
 
 
 def setup_driver():
-    driver = uc.Chrome(use_subprocess=True)
-    return driver
-    options = uc.ChromeOptions()
-    options.add_argument('--start-maximized')
-    options.add_argument('--disable-gpu')
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
+    options = Options()
+    options.add_argument("--headless")  # Run in headless mode
+    options.add_argument("--disable-gpu")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--window-size=1920x1080")
     options.add_argument(
         '--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36')
-    return uc.Chrome(options=options)
 
-
+    # Auto-install the correct ChromeDriver
+    service = Service(ChromeDriverManager().install())
+    return webdriver.Chrome(service=service, options=options)
 def is_properly_formatted_row(cells):
     """
     Check if the row matches the expected format of the ranking data
